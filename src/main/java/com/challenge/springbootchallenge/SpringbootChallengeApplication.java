a6c705626644de66ee836b961834b663b925021e
Christian-pprogrammer <mpanoc6@gmail.com>
2022-06-21 13:01:25 +0200

    first commit

---
+++ src/main/java/com/challenge/springbootchallenge/SpringbootChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.springbootchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/springbootchallenge/SpringbootChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.springbootchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
